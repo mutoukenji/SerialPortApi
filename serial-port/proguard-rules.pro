c46c4f85b064aac9b6d8806985e936ce44393196
mutoukenji <mutoukenji@gmail.com>
2019-11-13 15:25:20 +0800

    Add RTSCTS manual control

--- serial-port/proguard-rules.pro
+++ serial-port/proguard-rules.pro
@@ -2,4 +2,8 @@
 -keep class tech.yaog.hardwares.serialport.*{
     public *;
     public <methods>;
+    protected <methods>;
+}
+-keepclassmembernames class tech.yaog.hardwares.serialport.SerialPort {
+    private java.io.FileDescriptor mFd;
 }
--- serial-port/src/main/java/tech/yaog/hardwares/serialport/Bootstrap.java
+++ serial-port/src/main/java/tech/yaog/hardwares/serialport/Bootstrap.java
@@ -17,6 +17,43 @@ import java.util.concurrent.TimeUnit;
  * Created by mutoukenji on 17-8-12.
  */
 public class Bootstrap {
+
+    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+    private static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    public ReceiverStartEvent getReceiverStartEventListener() {
+        return receiverStartEventListener;
+    }
+
+    /**
+     * 注册接收进程开始事件监听
+     * @param receiverStartEventListener 接收进程开始事件监听
+     * @return self
+     */
+    public Bootstrap setReceiverStartEventListener(ReceiverStartEvent receiverStartEventListener) {
+        this.receiverStartEventListener = receiverStartEventListener;
+        return this;
+    }
+
+    /**
+     * 接收进程开始事件
+     */
+    public interface ReceiverStartEvent {
+        /**
+         * 接收进程开始
+         * @param bootstrap 对应的 Bootstrap
+         */
+        void receiverStarted(Bootstrap bootstrap);
+    }
+
     private static final String TAG = Bootstrap.class.getName();
 
     private SerialPort serialPort;
@@ -36,72 +73,145 @@ public class Bootstrap {
     private List<AbstractEncoder> encoders = new ArrayList<>();
     private List<AbstractHandler> handlers = new ArrayList<>();
 
+    private ReceiverStartEvent receiverStartEventListener;
+
+    /**
+     * Get device path
+     * @return device path
+     */
     public String getPath() {
         return path;
     }
 
+    /**
+     * Set device path
+     * @param path device path
+     */
     public void setPath(String path) {
         this.path = path;
     }
 
+    /**
+     * Get baudrate
+     * @return baudrate
+     */
     public int getBaudrate() {
         return baudrate;
     }
 
+    /**
+     * Set baudrate
+     * @param baudrate baudrate
+     */
     public void setBaudrate(int baudrate) {
         this.baudrate = baudrate;
     }
 
+    /**
+     * Get character size
+     * @return character size
+     */
     public int getCsize() {
         return csize;
     }
 
+    /**
+     * Set character size
+     * @param csize character size. Use {@link SerialPort#CSIZE_5}, {@link SerialPort#CSIZE_6}, {@link SerialPort#CSIZE_7}, {@link SerialPort#CSIZE_8}
+     */
     public void setCsize(int csize) {
         this.csize = csize;
     }
 
+    /**
+     * Get parity method
+     * @return parity method
+     */
     public int getParity() {
         return parity;
     }
 
+    /**
+     * Set parity method
+     * @param parity parity method. Use {@link SerialPort#PARITY_NONE}, {@link SerialPort#PARITY_ODD}, {@link SerialPort#PARITY_EVEN},{@link SerialPort#PARITY_SPACE}
+     */
     public void setParity(int parity) {
         this.parity = parity;
     }
 
+    /**
+     * Get stop bit
+     * @return stop bit
+     */
     public int getStopbits() {
         return stopbits;
     }
 
+    /**
+     * Set stop bit
+     * @param stopbits stop bit. Use {@link SerialPort#STOP_BIT_1} or {@link SerialPort#STOP_BIT_2}
+     */
     public void setStopbits(int stopbits) {
         this.stopbits = stopbits;
     }
 
+    /**
+     * Get extra flags while opening the device
+     * @return extra flags while opening the device
+     */
     public int getFlags() {
         return flags;
     }
 
+    /**
+     * Set extra flags while opening the device
+     * @param flags extra flags while opening the device
+     */
     public void setFlags(int flags) {
         this.flags = flags;
     }
 
+    /**
+     * Get whether auto hardware flow control is enabled
+     * @return whether auto hardware flow control is enabled
+     */
     public boolean isRtscts() {
         return rtscts;
     }
 
+    /**
+     * Get whether auto hardware flow control is enabled
+     * @param rtscts whether auto hardware flow control is enabled
+     */
     public void setRtscts(boolean rtscts) {
         this.rtscts = rtscts;
     }
 
+    /**
+     * Enable or disable verbose level log
+     * @param vvv {@code true} for enable
+     * @return self
+     */
     public Bootstrap vvv(boolean vvv) {
         this.vvv = vvv;
         return this;
     }
 
+    /**
+     * Set logger adapter
+     * @param logger logger adapter
+     * @return self
+     */
     public Bootstrap logger(Logger logger) {
         this.logger = logger;
         return this;
     }
 
+    /**
+     * Set decoder methods
+     * @param decoders decoder methods
+     * @return self
+     */
     public Bootstrap decode(AbstractDecoder... decoders) {
         for (AbstractDecoder decoder : decoders) {
             if (!this.decoders.contains(decoder)) {
@@ -111,6 +221,11 @@ public class Bootstrap {
         return this;
     }
 
+    /**
+     * Set encoder methods
+     * @param encoders encoder methods
+     * @return self
+     */
     public Bootstrap encode(AbstractEncoder... encoders) {
         for (AbstractEncoder encoder : encoders) {
             if (!this.encoders.contains(encoder)) {
@@ -120,6 +235,11 @@ public class Bootstrap {
         return this;
     }
 
+    /**
+     * Set data handlers
+     * @param handlers data handlers
+     * @return self
+     */
     public Bootstrap handle(AbstractHandler... handlers) {
         for (AbstractHandler handler : handlers) {
             if (!this.handlers.contains(handler)) {
@@ -129,10 +249,33 @@ public class Bootstrap {
         return this;
     }
 
+    /**
+     * Create {@code Bootstrap} with parameters
+     * Flow control is disabled
+     * @param path Device path
+     * @param baudrate Baudrate
+     * @param csize Characters size
+     * @param parity Parity method
+     * @param stopbits Stop bit
+     * @param flags Extra flags while opening device
+     * @return new Bootstrap instance
+     */
     public Bootstrap configure(String path, int baudrate, int csize, int parity, int stopbits, int flags) {
         return configure(path, baudrate, csize, parity, stopbits, false, false, flags);
     }
 
+    /**
+     * Create {@code Bootstrap} with parameters
+     * @param path Device path
+     * @param baudrate Baudrate
+     * @param csize Characters size
+     * @param parity Parity method
+     * @param stopbits Stop bit
+     * @param rtscts Hardware flow control enable or not
+     * @param xonxoff Software flow control enable or not
+     * @param flags Extra flags while opening device
+     * @return new Bootstrap instance
+     */
     public Bootstrap configure(String path, int baudrate, int csize, int parity, int stopbits, boolean rtscts, boolean xonxoff, int flags) {
         this.path = path;
         this.baudrate = baudrate;
@@ -145,6 +288,11 @@ public class Bootstrap {
         return this;
     }
 
+    /**
+     * Send message
+     * @param message message to be sent
+     * @return self
+     */
     public Bootstrap send(Object message) {
         byte[] data = null;
         if (message instanceof byte[]) {
@@ -171,7 +319,7 @@ public class Bootstrap {
             try {
                 serialPort.getOutputStream().write(data);
                 if (vvv && logger != null) {
-                    logger.v(TAG, "Tx: %s", Arrays.toString(data));
+                    logger.v(TAG, "Tx: %s", bytesToHex(data));
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -184,6 +332,32 @@ public class Bootstrap {
         return this;
     }
 
+    /**
+     * Set rtx mark manually
+     * @param mark rtx status
+     */
+    public void setRtx(boolean mark) {
+        if (serialPort != null) {
+            serialPort.setRtx(mark);
+        }
+    }
+
+    /**
+     * Get ctx mark manually
+     * @return ctx status
+     */
+    public boolean getCtx() {
+        if (serialPort != null) {
+            return serialPort.getCtx();
+        }
+        return false;
+    }
+
+    /**
+     * Open serial port and start handling
+     * @return self
+     * @throws IOException serial port open failed
+     */
     public Bootstrap start() throws IOException {
         //创建等待队列
         BlockingQueue<Runnable> bqueue = new ArrayBlockingQueue<>(20);
@@ -217,7 +391,7 @@ public class Bootstrap {
                         while (!isPack);
                         if (data.length > 0) {
                             if (vvv && logger != null) {
-                                logger.v(TAG, "Rx: %s", Arrays.toString(data));
+                                logger.v(TAG, "Rx: %s", bytesToHex(data));
                             }
                             final byte[] h_data = data;
                             workgroup.execute(new Runnable() {
@@ -238,10 +412,16 @@ public class Bootstrap {
         receiveThread.setName(path + " Receiver");
         receiveThread.setPriority(Thread.MAX_PRIORITY);
         receiveThread.start();
+        if (receiverStartEventListener != null) {
+            receiverStartEventListener.receiverStarted(this);
+        }
 
         return this;
     }
 
+    /**
+     * Close serial port and stop all handling
+     */
     public void stop() {
         receiveThread.interrupt();
         workgroup.shutdown();
@@ -284,10 +464,18 @@ public class Bootstrap {
         }
     }
 
+    /**
+     * Get whether software flow control is enabled
+     * @return whether software flow control is enabled
+     */
     public boolean isXonxoff() {
         return xonxoff;
     }
 
+    /**
+     * Set whether software flow control is enabled
+     * @param xonxoff whether software flow control is enabled
+     */
     public void setXonxoff(boolean xonxoff) {
         this.xonxoff = xonxoff;
     }
