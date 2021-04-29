464c9811a676ce9765229eefe35a9f49ccb60037
mutoukenji <mutoukenji@gmail.com>
2021-04-29 10:56:36 +0800

    更新依赖

--- app/src/main/java/tech/yaog/hardwares/serialport/sample/HelloCommand.java
+++
@@ -1,9 +0,0 @@
-package tech.yaog.hardwares.serialport.sample;
-
-/**
- * Created by ygl_h on 2017/9/9.
- */
-
-public class HelloCommand {
-    public static final byte ID = 0x18;
-}
--- app/src/main/java/tech/yaog/hardwares/serialport/sample/SelectCommand.java
+++
@@ -1,9 +0,0 @@
-package tech.yaog.hardwares.serialport.sample;
-
-/**
- * Created by ygl_h on 2017/9/10.
- */
-
-public class SelectCommand {
-    public static final int ID = 0x1B;
-}
